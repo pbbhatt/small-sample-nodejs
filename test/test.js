805ae8821b019cf100956a9278a5ccf969d4ee53
pinakin bhatt <bhatt.pinakin@hotmail.com>
2021-01-13 15:11:59 +0530

    sixth commit

--- test/test.js
+++ test/test.js
@@ -1,8 +1,25 @@
-var request = require(‘supertest’);
-var app = require(‘../index.js’);
-describe(‘GET /’, function() {
- it(‘respond with hello world’, function(done) {
- //navigate to root and check the the response is "hello world"
- // request(app).get(‘/’).expect(‘hello world’, done);
-  });
-  });
+var supertest = require("supertest");
+var should = require("should");
+
+// This agent refers to PORT where the program is running.
+//
+ var server = supertest.agent("http://localhost:4000");
+//
+// // UNIT test begin
+//
+ describe("SAMPLE unit test",function(){
+//
+   // #1 should return home page
+     it("should return home page",function(done){
+//         // calling home page
+             server
+                 .get("/")
+                     .expect("Content-type",/text/)
+                         .expect(200) // THis is HTTP response
+                             .end(function(err,res){
+//                                   // HTTP status should be 200
+                                         res.status.should.equal(200);
+                                               done();
+                                                   });
+                                                     });
+                                                     });
